7f51284010e756655d903562b8d9c157f4c3339a
JoshDaos <joshuadaos1998@gmail.com>
2021-07-15 13:03:18 -0700

    added typewriter-effect

--- portfolio-app/src/pages/Home.js
+++ portfolio-app/src/pages/Home.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect';
 
 function Home() {
     return (
         <div>
-            <h1>Josh Daos</h1>
+            <Typewriter className="typewriter__content"
+            onInit={(typewriter) => {
+                typewriter.typeString('Creator')
+                .callFunction(() => {
+                    console.log('String typed out!');
+                })
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString('Designer')
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString('Developer')
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString('Josh Daos')
+                .callFunction(() => {
+                    console.log('All strings were deleted');
+                })
+                .start();
+            }}
+            />
+
+
             <p className="home__p-one">
                 A 23-year-old <u>Software Engineer</u>, Bay Area based.<br></br>
             </p>
