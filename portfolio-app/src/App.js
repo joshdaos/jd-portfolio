b7eb00a8a4887688ce8cab5c9e96cac0f4274820
JoshDaos <joshuadaos1998@gmail.com>
2021-07-14 20:13:26 -0700

    added two routes & nav

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>Portfolio Site</h1>
+    <Router>
+
+
+    <div className="app__container">
+      <h1>Portfolio Site</h1>
+      
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+      </Switch>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
     </div>
+
+    </Router>
   );
 }
 
---
+++ portfolio-app/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                A 23-year-old software engineer, bay area based.<br></br>
+                Creator. Designer. Developer.
+            </p>
+        </div>
+    )
+}
+
+export default Home
